a18c0bdf2af15a7d8ea613c6cebfe589e3f60996
Lucas Esteves <83038670+luqui2@users.noreply.github.com>
2021-07-28 12:45:11 -0300

    Add files via upload

---
+++ BANCO DE DADOS (COMANDOS).sql
@@ -0,0 +1,68 @@
+create table administrador(
+
+login varchar(100) primary key,
+senha varchar(100)
+
+	
+	
+	
+);
+
+create table Paciente(
+
+idPaciente serial primary key,
+nome varchar(100) ,
+cpf varchar(15) UNIQUE,
+endereco varchar(100),
+Email varchar(100),
+
+);
+
+
+create table Medico(
+
+idMedico serial primary key,
+nome varchar(100) ,
+cpf varchar(15) UNIQUE,
+especialidade varchar(100),
+email varchar(100),
+sexo varchar(50)
+
+
+);
+
+
+
+
+create table Agenda(
+idAgenda serial primary key
+
+horario varchar(50),
+data varchar (50);
+
+id_Medico int,
+
+id_Paciente int,
+
+FOREIGN KEY(id_Medico) 
+REFERENCES Medico(idMedico) 
+
+FOREIGN KEY(id_Paciente) 
+REFERENCES Paciente(idPaciente) 
+
+);
+
+
+select agenda.horario,paciente.nome, medico.nome, agenda.procedimento, agenda.data from paciente,medico,agenda
+
+where idpaciente=Id_paciente and idMedico = id_medico;
+
+
+
+
+
+select login from administrador where =?
+
+insert into administrador (login,senha) values ("admin","admin");
+
+
